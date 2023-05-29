b59bc72239620e63152d140ac8b410c4a58c0c60
Herobrine <123895209+DevHerobrine@users.noreply.github.com>
2023-05-29 16:22:37 +0200

    Fix bug

--- Scripthub.lua
+++ Scripthub.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 12218138312 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/DevHerobrine/Script/main/StreamerLife/StreamerLife.lua',true))()   -- Streamer life
+     loadstring(game:HttpGet('https://raw.githubusercontent.com/DevHerobrine/Script/main/StreamerLife/StreamerLife.lua',true))()   -- Streamer life
 end
