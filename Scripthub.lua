c750b8144673a6fa3086f854e1cb77e53a0b53b4
Herobrine <123895209+DevHerobrine@users.noreply.github.com>
2023-09-09 18:29:31 +0200

    Update Scripthub.lua

--- Scripthub.lua
+++ Scripthub.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 12218138312 then
      loadstring(game:HttpGet('https://raw.githubusercontent.com/DevHerobrine/Script/main/StreamerLife/StreamerLife.lua',true))()   -- Streamer life
 end
+if game.PlaceId == 5081773298 then
+     loadstring(game:HttpGet('https://raw.githubusercontent.com/DevHerobrine/Script/main/NoScope/NoScope.lua',true))() -- no scope
+end
